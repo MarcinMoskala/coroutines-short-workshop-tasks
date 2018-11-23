52c03faaf8dc2e31d98e66fac53f8d853b7cc76f
Marcin Moskala <marcinmoskala@gmail.com>
2018-11-23 14:54:04 +0700

    Fix example

--- src/examples/1.kt
+++ src/examples/1.kt
@@ -3,15 +3,11 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-//fun main() {
-//    GlobalScope.launch {
-//        delay(1000L)
-//        println("World!")
-//    }
-//    println("Hello,")
-//    Thread.sleep(2000L)
-//}
-
 fun main() {
-    print("Hello, world")
+    GlobalScope.launch {
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,")
+    Thread.sleep(2000L)
 }
